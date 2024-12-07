315ab292a0ce75bbe3f55d5b264e4e7614d48034
Kate Jensen <kmjens@umich.edu>
2024-12-07 16:57:29 -0500

    we don t know

--- src/lattice.py
+++ src/lattice.py
@@ -29,8 +29,8 @@ def make_bravais2d(n, L2=1.0, theta=np.pi / 2, centered=False, sigma_noise=0.0):
     if centered:
         assert theta == np.pi / 2, "Nonrectangular centered lattices are not valid!"
 
-    L1, α, β = 1, np.pi / 2, np.pi / 2
-    box = freud.box.Box.from_box_lengths_and_angles(L1, L2, 0, α, β, theta)
+    L1, alpha, beta = 1, np.pi / 2, np.pi / 2
+    box = freud.box.Box.from_box_lengths_and_angles(L1, L2, 0, alpha, beta, theta)
     #box = freud.box.Box(Lx=L1, Ly=L2, is2D=True, tilt_factors=(0, theta))
     basis_positions = [[0.5, 0.5, 0], [0.0, 0.0, 0]] if centered else [[0.5, 0.5, 0]]
 
@@ -38,37 +38,53 @@ def make_bravais2d(n, L2=1.0, theta=np.pi / 2, centered=False, sigma_noise=0.0):
     box, pos = uc.generate_system(n, sigma_noise=sigma_noise, seed=SEED)
     return box, pos - pos.mean(axis=0)
 
-
-def lattice2image(box, pos, px=64, blur_sigma=0.0, pad_width=0):
-    """Convert a freud system to a 2D image with an optional Gaussian blur."""
-
-    # Calculate rectangular image with aspect ratio matching the input box
+def lat2im_KT(box, pos, px=64, blur_sigma=0.0, pad_width=0):
     assert np.isclose(box.xy, 0.0), "Box should be rectangular to bin correctly!"
-    px = (
-        px,
-        int(box.Ly / box.Lx * px),
-    )  # if box.Ly > box.Lx else (int(box.Lx / box.Ly * px), px)
+    bin_width = box.Lx / px
+    x_max = max(pos[:,0])
+    x_min = min(pos[:,0])
+    x_bins = bin_width * np.arange(np.floor(x_min / bin_width), np.ceil(x_max / bin_width) + 1)
+    print('px: ',px)
+    print('len: ',len(x_bins))
+    #print(x_bins)
+    y_max = max(pos[:,1])
+    y_min = min(pos[:,1])
+    y_bins = bin_width * np.arange(np.floor(y_min / bin_width), np.ceil(y_max / bin_width) + 1)
+    
+    '''
+    plt.figure(figsize=(80,80))
+    plt.hlines(y_bins, min(pos[:,0]), max(pos[:,0]), color='k', alpha=.5)
+    plt.vlines(x_bins, min(pos[:,1]), max(pos[:,1]), color='k', alpha=.5)
+    plt.scatter(pos[:, 0], -pos[:, 1])
+    #plt.scatter(x,np.zeros_like(x))
+    plt.axis('equal')
+    plt.savefig(f"../figs/pixelspace_NEW_{pos[3,0]:.2f}.png")
+    plt.close()
+    '''
 
-    image, x, y = np.histogram2d(pos[:, 0], pos[:, 1], bins=px)
+    image, x, y = np.histogram2d(pos[:, 0], pos[:, 1], bins=(x_bins,y_bins))
     image = image.T
-    x = x[:-1] + np.diff(x)
-    y = y[:-1] + np.diff(y)
-
+    '''
+    plt.figure()
+    plt.set_cmap('cmc.acton')
+    plt.imshow(image, aspect="equal")
+    plt.axis('equal')
+    plt.savefig(f"../figs/KT_hist_{pos[3,0]:.2f}.png",transparent=True, bbox_inches="tight")
+    plt.close()
+    '''
     # Remove points from outside the box. Should never trigger for orthogonal boxes
-    BOX_PADDING_SCALE = 0.9  # Required to properly wrap points near the box edges
-    coords = np.asarray(np.meshgrid(x, y)).reshape(2, -1).T
-    coords = np.hstack([coords, np.zeros((coords.shape[0], 1))])
-    is_inside = ~box.contains(BOX_PADDING_SCALE * coords).reshape(px).T
-    image[is_inside] = np.nan
+    if not np.isclose(box.xy,0.0):
+        BOX_PADDING_SCALE = 0.9  # Required to properly wrap points near the box edges
+        coords = np.asarray(np.meshgrid(x, y)).reshape(2, -1).T
+        coords = np.hstack([coords, np.zeros((coords.shape[0], 1))])
+        is_inside = ~box.contains(BOX_PADDING_SCALE * coords).reshape(px).T
+        image[is_inside] = np.nan
 
     image = np.pad(
         image,
         pad_width=pad_width,
         constant_values=np.nan if np.isnan(image).any() else 0,
     )
-
-    # TODO: image does not handle boundaries properly: real image is not square
-    # image = sp.ndimage.gaussian_filter(image, sigma=blur_sigma, mode="wrap")
     image = sp.ndimage.gaussian_filter(image, sigma=blur_sigma, mode="constant")
 
     return image.astype(np.float32)
@@ -96,17 +112,16 @@ def gkern(l=5, sigma=1.0):  # noqa: E741
 
 
 #def bravais_kernel(L2=1.0, theta=np.pi / 2, centered=False, blur_sigma=0.0, px=9, n=(3,3)):
-def bravais_kernel(L2=1.0, theta=np.pi / 2, centered=False, blur_sigma=0.0, px=9, n=(3,3), rot_angle = 0):
+def bravais_kernel(L2=1.0, theta=np.pi / 2, centered=False, blur_sigma=0.0, n=(3,3), rot_angle = 0, px_width=1):
     """Create a one unit-cell kernel from bravais lattice parameters."""
     print(f"L2: {L2}")
     print(f"theta: {theta/np.pi/np.pi:.4f}π")
-    print(f"px: {px}")
+    #print(f"px: {px}")
     box, pos = make_bravais2d(n=(n[0]+2, n[1]+2, 1), L2=L2, theta=theta, centered=centered)
     print('len pos 0: ', len(pos))
     #box, pos, _ = slice_to_orthogonal(
     #    *make_bravais2d(n=(*n, 1), L2=L2, theta=theta, centered=centered))
     
-
     box, unrot_pos, _ = slice_to_orthogonal(box, pos)
     
     #pos[:,:-1] = unrot_pos[:,:-1] @ rotation_matrix(rot_angle).T
@@ -115,8 +130,9 @@ def bravais_kernel(L2=1.0, theta=np.pi / 2, centered=False, blur_sigma=0.0, px=9
     print('rot_angle: ', rot_angle)
     
     '''
-    plt.scatter(unrot_pos[:,0],-unrot_pos[:,1], s=.5)
-    plt.scatter(pos[:,0],-pos[:,1], s=.5)
+    plt.scatter(unrot_pos[:,0],-unrot_pos[:,1], s=1)
+    plt.scatter(pos[:,0],-pos[:,1], s=1)
+    plt.axis('equal')
     plt.savefig(f"../figs/kpos_{rot_angle:.2f}.png",transparent=True, dpi=300, bbox_inches="tight")
     plt.close()
     '''
@@ -129,13 +145,21 @@ def bravais_kernel(L2=1.0, theta=np.pi / 2, centered=False, blur_sigma=0.0, px=9
     print(pos) 
     print(box)
     print('len pos 1: ', len(pos))
-
+    
     '''
-    plt.scatter(pos[:,0], pos[:,1])
-    plt.savefig(f'../figs/example2_pts_final_{theta:.2f}.png',transparent=True, bbox_inches="tight")
+    plt.scatter(pos[:,0], -pos[:,1])
+    plt.axis('equal')
+    plt.savefig(f'../figs/example2_pts_final_{rot_angle:.2f}.png',transparent=True, bbox_inches="tight")
     plt.close()
     '''
-    return lattice2image(box, pos, px=px, blur_sigma=blur_sigma, pad_width=4)
+
+    x_min = min(pos[:,0])
+    x_max = max(pos[:,0])
+
+    px = np.ceil(x_max/px_width - x_min/px_width) # number of pixels depends on rotation of point before image
+
+    #return lattice2image(box, pos, px=px, blur_sigma=blur_sigma, pad_width=4)
+    return lat2im_KT(box, pos, px=px, blur_sigma=blur_sigma, pad_width=4)
 
 
 def frame2image(
@@ -187,15 +211,21 @@ def make_polycrystalline(midpoints, theta, n=100, L2=1.0, sigma_noise=0.0):
             sigma_noise=sigma_noise
         )
 
+        
         #region_pos[:,:-1] = region_pos[:,:-1] @ rot_matrix.T
         region_pos[:,:-1] = region_pos[:,:-1] @ rot_matrix
         
         translated_pos = region_pos #+ midpoint
+
         '''
-        plt.scatter(translated_pos[:,0],-translated_pos[:,1], s=.4)
-        plt.savefig("../figs/pos_before_cut.png",transparent=True, dpi=300, bbox_inches="tight")
+        plt.scatter(translated_pos[:,0],-translated_pos[:,1], s=.3)
+        plt.axis('equal')
+        plt.xlim(0,4)
+        plt.ylim(-2,2)
+        plt.savefig("../figs/pos_before_cut.png",transparent=True, dpi=500, bbox_inches="tight")
         plt.close()
         '''
+        
         distances = box.compute_all_distances(translated_pos, midpoints)
         
         # Filter points within Voronoi region
@@ -206,12 +236,13 @@ def make_polycrystalline(midpoints, theta, n=100, L2=1.0, sigma_noise=0.0):
         all_positions.append(wrapped_pos)
     
     rotations[0] = 0
-    print('rotations:\n', rotations)
+    print('rotations end of makepoly:\n', rotations)
 
     # Combine all positions
     final_positions = np.vstack(all_positions)
     '''
     plt.scatter(final_positions[:,0],-final_positions[:,1], s=.4)
+    plt.axis('equal')
     plt.savefig("../figs/pos_after_cut.png",transparent=True, dpi=300, bbox_inches="tight")
     plt.close()
     '''
@@ -223,6 +254,62 @@ def make_polycrystalline(midpoints, theta, n=100, L2=1.0, sigma_noise=0.0):
     return final_box, final_positions, rotations
 
 
+def lattice2image(box, pos, px=64, blur_sigma=0.0, pad_width=0):
+    """Convert a freud system to a 2D image with an optional Gaussian blur."""
+
+    # Calculate rectangular image with aspect ratio matching the input box
+    assert np.isclose(box.xy, 0.0), "Box should be rectangular to bin correctly!"
+    px = (px, int(box.Ly / box.Lx * px),)  
+    # if box.Ly > box.Lx else (int(box.Lx / box.Ly * px), px)
+    '''
+    plt.scatter(pos[:, 0], -pos[:, 1]) 
+    plt.axis('equal')
+    plt.savefig(f"../figs/Porfavormantegasusmanos{pos[3,0]:.2f}.png")
+    plt.close()
+    '''
+    image, x, y = np.histogram2d(pos[:, 0], pos[:, 1], bins=px)
+    image = image.T
+    x = x[:-1] + np.diff(x)
+    y = y[:-1] + np.diff(y)
+    print(np.diff(x))
+
+
+    # Remove points from outside the box. Should never trigger for orthogonal boxes
+    BOX_PADDING_SCALE = 0.9  # Required to properly wrap points near the box edges
+    ''' 
+    plt.figure(figsize=(20,20))
+    plt.hlines(y,min(pos[:,1]),max(pos[:,1]), color='k', alpha=.5) 
+    plt.vlines(x,min(pos[:,0]),max(pos[:,0]), color='k', alpha=.5)
+    plt.scatter(pos[:, 0], -pos[:, 1]) 
+    #plt.scatter(x,np.zeros_like(x))
+    plt.axis('equal')
+    plt.savefig(f"../figs/pixelspace_{pos[3,0]:.2f}.png")
+    plt.close()
+    
+    plt.figure()
+    plt.set_cmap('cmc.acton')
+    plt.imshow(image, aspect="equal")
+    plt.axis('equal')
+    plt.savefig(f"../figs/example2_image_b4pad_{pos[3,0]:.2f}.png",transparent=True, bbox_inches="tight")
+    plt.close()
+    '''
+    coords = np.asarray(np.meshgrid(x, y)).reshape(2, -1).T
+    coords = np.hstack([coords, np.zeros((coords.shape[0], 1))])
+    is_inside = ~box.contains(BOX_PADDING_SCALE * coords).reshape(px).T
+    image[is_inside] = np.nan
+
+    image = np.pad(
+        image,
+        pad_width=pad_width,
+        constant_values=np.nan if np.isnan(image).any() else 0,
+    )
+    
+
+    # TODO: image does not handle boundaries properly: real image is not square
+    # image = sp.ndimage.gaussian_filter(image, sigma=blur_sigma, mode="wrap")
+    image = sp.ndimage.gaussian_filter(image, sigma=blur_sigma, mode="constant")
+
+    return image.astype(np.float32)
 #midpoints = np.array([[-0.45,-0.25,0],[-0.4,0.25,0],[0.35,0.35,0]])
 #make_polycrystalline(midpoints, theta =2*np.pi/3, n=30, L2=1.0, sigma_noise=0.0)
 
